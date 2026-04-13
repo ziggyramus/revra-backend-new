6d57f364b34ad149bf5fe81b414a5cca29d3baec
ziggyramus <141076553+ziggyramus@users.noreply.github.com>
2026-04-13 19:47:16 -0400

    setup production build

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import app from "./app";
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-console.log(`Server running on port ${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
