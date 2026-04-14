1a18ed7fd2d656538acb4536b43d32c8ea2f4b9d
ziggyramus <141076553+ziggyramus@users.noreply.github.com>
2026-04-13 20:03:19 -0400

    fix port type issue

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import app from "./app";
 
-const PORT = process.env.PORT || 5000;
+const PORT = Number(process.env.PORT) || 5000;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
